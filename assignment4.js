36d9761a43da253c72af52e3894004dbb8dd0b87
Sancharita Dutta <77744537+bliss22@users.noreply.github.com>
2021-01-26 20:39:43 +0530

    Add files via upload
    This is my assignment 4 for JS ESSENTIALS.

---
+++ assignment4.js
@@ -0,0 +1,11 @@
+// Q2
+function sum()
+{
+    var i=document.getElementById("num1")
+    var num1=i.value
+    var j=document.getElementById("num2")
+    var num2=j.value
+    var c=num1+num2
+    console.log("The Sum of the numbers is:",c)
+    
+}
